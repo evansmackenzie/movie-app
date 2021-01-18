a1c7927b3068f0dc309f5da2ab295e052071f9fd
mackenzie <evans.mackenzie@gmail.com>
2021-01-18 10:55:53 -0500

    adds queery parameters

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -2,6 +2,10 @@ class Api::ActorsController < ApplicationController
   def single_actor_action
     @single_actor = Actor.find_by(id: 1)
     render "single_actor.json.jb"
-    
+  end
+
+  def choose_actor_action
+    @actor = params[:actor]
+    render "choose_actor.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
     get "actor_path" => "actors#single_actor_action"
     get "all_movies_path" => "movies#all_movies_action"
     get "first_movie_path" => "movies#first_movie_action"
+    get "choose_actor_path" => "actors#choose_actor_action"
+    get "choose_actor_path/:actor" => "actors#choose_actor_action"
   end
 end
