35ff4d7abe5f7d6cc97cfa72f2f0921bd5ba892e
mackenzie <evans.mackenzie@gmail.com>
2021-01-20 19:59:11 -0500

    updates movie app migrations

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -20,10 +20,12 @@ class Api::ActorsController < ApplicationController
   end
 
   def create
-    @actor = actor.create(
+    @actor = Actor.create(
       first_name: params[:first_name],
       last_name: params[:last_name],
       known_for: params[:known_for],
+      gender: params[:gender],
+      age: params[:age]
     )
     render "show.json.jb"
   end
@@ -33,6 +35,8 @@ class Api::ActorsController < ApplicationController
     @actor.first_name = params[:first_name] ||  @actor.first_name
     @actor.last_name = params[:last_name] ||  @actor.last_name
     @actor.known_for = params[:known_for] ||  @actor.known_for
+    @actor.gender = params[:gender] || @actor.gender
+    @actor.age = params[:age] || @actor.age
     @actor.save
     render "show.json.jb"
   end
